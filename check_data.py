d1ba8de484439cf9cfc890be8765b0d647e02abf
Ajay051198 <ajay.uom@gmail.com>
2020-10-17 12:31:16 +0530

    Single class classification attempt

--- check_data.py
+++ check_data.py
@@ -1,30 +1,35 @@
 import numpy as np
-file_name = input("enter file name")
+file_name = input("enter file name: ")
 path = f"./PP_Data/{file_name}"
 
-X = np.load(f"{path}_X.npy", allow_pickle=True)
-Y = np.load(f"{path}_Y.npy", allow_pickle=True)
-enc_dec = np.load(f"{path}_ENCDEC.npy", allow_pickle=True)
-series = np.load(f"{path}_series.npy", allow_pickle=True)
+single = int(input('Binary Task (1/0): '))
+
+if single:
+    X = np.load(f"{path}_X_Single.npy", allow_pickle=True)
+    Y = np.load(f"{path}_Y_Single.npy", allow_pickle=True)
+    enc_dec = np.load(f"{path}_ENCDEC_Single.npy", allow_pickle=True)
+else:
+    X = np.load(f"{path}_X.npy", allow_pickle=True)
+    Y = np.load(f"{path}_Y.npy", allow_pickle=True)
+    enc_dec = np.load(f"{path}_ENCDEC.npy", allow_pickle=True)
 
 (idx2act, idx2obs, act2idx, obs2idx) = enc_dec
 
 
-#Test1
-
-idx = int(input('Enter index: '))
-print('-'*88)
-print(f"X-> {X[idx]}, tot: {len(X[idx])}")
-print(f"Y-> {Y[idx]}")
+# Test1
+# idx = int(input('Enter index: '))
+# print('-'*88)
+# print(f"X-> {X[idx]}, tot: {len(X[idx])}")
+# print(f"Y-> {Y[idx]}")
 # print(act2idx)
 
-#Test2
+# Test2
 # print(len(series))
-# print(series[:50])
 
-#Test 3
-print(idx2act)
+# Test 3
 print(f"Size >> {len(X)}")
 print()
-print("ENCODING DIM >> ")
-print(len(set(obs2idx.values())))
+print(idx2act)
+print(np.unique(Y))
+print()
+print(f"ENCODING DIM >> {len(set(obs2idx.values()))}")
---
+++ prepare_data_single.py
@@ -0,0 +1,212 @@
+# Data-preprocessing steps to prepare data for DL models
+
+# imports
+import config
+import os
+import re
+import numpy as np
+from keras.preprocessing import sequence
+from datetime import datetime
+from tqdm import tqdm
+
+categorizeActivites = {
+    'cairo': {
+        "": "Other",
+        "Bed to toilet": "Use Toiltet",
+        "R1 wake": "Wake",
+        "R2 wake": "Wake",
+        "Night wandering": "Other",
+        "R1 work in office": "Work",
+        "Laundry": "Work",
+        "R2 take medicine": "Take_medicine",
+        "R1 sleep": "Sleep",
+        "R2 sleep": "Sleep",
+        "Leave home": "Leave_Home",
+        "Breakfast": "Eat",
+        "Dinner": "Eat",
+        "Lunch": "Eat",
+    },
+    "milan": {
+        "": "Other",
+        "Master_Bedroom_Activity": "Master",
+        "Meditate": "Meditate",
+        "Chores": "Work",
+        "Desk_Activity": "Work",
+        "Morning_Meds": "Take_medicine",
+        "Eve_Meds": "Take_medicine",
+        "Sleep": "Sleep",
+        "Read": "Read",
+        "Watch_TV": "Watch_TV",
+        "Leave_Home": "Leave_Home",
+        "Dining_Rm_Activity": "Eat",
+        "Kitchen_Activity": "Cook",
+        "Bed_to_Toilet": "Use Toilet",
+        "Master_Bathroom": "Master_Bath_use",
+        "Guest_Bathroom": "Guest_Bath_use"},
+
+    "kyoto7": {
+        "R1_Bed_to_Toilet": "Use Toilet",
+        "R2_Bed_to_Toilet": "Use Toilet",
+        "Meal_Preparation": "Cook",
+        "R1_Personal_Hygiene": "Personal_hygiene",
+        "R2_Personal_Hygiene": "Personal_hygiene",
+        "Watch_TV": "Watch TV",
+        "R1_Sleep": "Sleep",
+        "R2_Sleep": "Sleep",
+        "Clean": "Other",
+        "R1_Work": "Work",
+        "R2_Work": "Work",
+        "Study": "Study",
+        "Wash_Bathtub": "Other",
+        "": "Other"},
+}
+
+
+def load_data(filename):
+    timestamps = []
+    sensors = []
+    values = []
+    activities = []
+
+    activity = ''
+
+    print('Loading File ...')
+    with open(filename, 'rb') as file:
+        data = file.readlines()
+        for i, line in tqdm(enumerate(data), total=len(data)):
+            sample = line.decode().split()
+            try:
+                if 'M' == sample[2][0] or 'D' == sample[2][0] or 'T' == sample[2][0]:
+                    # choose only M D T sensors, avoiding unexpected errors
+                    if not ('.' in str(np.array(sample[0])) + str(np.array(sample[1]))):
+                        sample[1] = sample[1] + '.000000'
+                    timestamps.append(datetime.strptime(str(np.array(sample[0])) + str(np.array(sample[1])),
+                                                        "%Y-%m-%d%H:%M:%S.%f"))
+                    sensors.append(str(np.array(sample[2])))
+                    values.append(str(np.array(sample[3])))
+
+                    if len(sample) == 4:  # if activity does not exist
+                        activities.append(activity)
+                    else:  # if activity exists
+                        des = str(' '.join(np.array(sample[4:])))
+                        if 'begin' in des:
+                            activity = re.sub('begin', '', des)
+                            activity = activity.rstrip()
+                            activities.append(activity)
+                        if 'end' in des:
+                            activities.append(activity)
+                            activity = ''
+            except IndexError:
+                print(i, line)
+    file.close()
+
+    # unique values of temperature
+    temperature = []
+    for element in values:
+        try:
+            temperature.append(float(element))
+        except ValueError:
+            pass
+
+    # unique sensors
+    sensorList = sorted(set(sensors))
+
+    # mapping sensor activations
+    obs2idx = {}
+    count = 0
+
+    print('Preprocessing ..')
+
+    for key in tqdm(sensorList, total=len(sensorList)):
+        if "M" or "AD" in key:
+            obs2idx[key + "OFF"] = count
+            count += 1
+            obs2idx[key + "ON"] = count
+            count += 1
+        if "D" in key:
+            obs2idx[key + "CLOSE"] = count
+            count += 1
+            obs2idx[key + "OPEN"] = count
+            count += 1
+        if "T" in key:
+            for temp in range(0, int((max(temperature) - min(temperature)) * 2) + 1):
+                obs2idx[key + str(float(temp / 2.0) +
+                                  min(temperature))] = count + temp
+
+    idx2obs = [x for x in obs2idx.keys()]
+
+    catActivies = [categorizeActivites[filename.split(
+        '/')[-1]][i] for i in activities]
+
+    idx2act = sorted(set(catActivies))
+    act2idx = {a: i for i, a in enumerate(idx2act)}
+
+    ## Select activity
+    act_no = act2idx['Take_medicine']
+
+    Xi = []
+    Yi = []
+
+    for _entry, sensor in tqdm(enumerate(sensors), total=len(sensors)):
+        if "T" in sensor:
+            Xi.append(obs2idx[sensor + str(round(float(values[_entry]), 1))])
+        else:
+            Xi.append(obs2idx[sensor + str(values[_entry])])
+        Yi.append(act2idx[catActivies[_entry]])
+
+    X = []
+    Y = []
+
+    for i, y in tqdm(enumerate(Yi), total=len(Yi)):
+        if i == 0:
+            Y.append(y)
+            x = [Xi[i]]
+        if i > 0:
+            if y == Yi[i - 1]:
+                x.append(Xi[i])
+            else:
+                Y.append(y)
+                X.append(x)
+                x = [Xi[i]]
+        if i == len(Yi) - 1:
+            if y != Yi[i - 1]:
+                Y.append(y)
+            X.append(x)
+    
+    for i, each in enumerate(Y):
+        if each != act_no:
+            Y[i] = 0
+        else: 
+            Y[i] = 1
+
+    act2idx = {
+        'Other': 0,
+        'Take_medicine' : 1,
+    }
+
+    idx2act = ['Other', 'Take_medicine']
+
+    return X, Y, (idx2act, idx2obs, act2idx, obs2idx)
+
+
+if __name__ == "__main__":
+    print('Preparing data')
+    name = input('Enter dataset name: ')
+    file = f"./Data/{name}"
+    print(f"File >> {file}")
+
+    X, Y, enc_dec = load_data(file)
+    print('Saving file')
+    # convert to numpy arrays
+    X = np.array(X, dtype=object)
+    Y = np.array(Y, dtype=object)
+    enc_dec = np.array(enc_dec, dtype=object)
+    X = sequence.pad_sequences(X, maxlen=config.MAX_LEN, dtype=np.int32)
+
+    save_to = f"./PP_Data/{file.split('/')[-1]}"
+
+    np.save(save_to + '_X_Single.npy', X)
+    np.save(save_to + '_Y_Single.npy', Y)
+    np.save(save_to + '_ENCDEC_Single.npy', enc_dec)
+
+    print('done')
